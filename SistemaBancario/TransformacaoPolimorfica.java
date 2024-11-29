15b91e17b8e29be1a0d6d327845276a6a4458abb
Eli <164237237+elionayx@users.noreply.github.com>
2024-11-29 15:19:29 -0300

    Delete SistemaBancario/TransformacaoPolimorfica.java

--- SistemaBancario/TransformacaoPolimorfica.java
+++
@@ -1,8 +0,0 @@
-public class TransformacaoPolimorfica {
-    public void realizarsaque (TransacoesBancarias cliente, double valor) {
-        cliente.sacar(valor);
-    }
-    public void realizardeposito (TransacoesBancarias cliente, double valor) {
-        cliente.depositar(valor);
-    }
-}
